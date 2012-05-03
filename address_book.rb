c4fa3d815af1f955db48a3bed761af71122bae8e
Andrew Peacock <skooljster@gmail.com>
2012-05-02 23:05:52 -0500

    Stuff actually works

--- address_book.rb
+++ address_book.rb
@@ -1,7 +1,7 @@
 require_relative 'address_entry'
 
 book = Array.new
-
+#hash = Hash.new
 #This reads file
 File.open('book.txt') do |f|
   count = f.gets.chop.to_i
@@ -25,7 +25,37 @@ def List(book)
   end
 end
 
-#HELLO
+commands = {
+  1 => ->(p1,p2) {...},
+  2 => ->(p1,p2) {...},
+  3 => ->(p1,p2) {...},
+}
+
 
 #MAIN
-List(book)
+b = 1
+begin
+  print "Command?: "
+  com = gets.chop.to_i
+  hash[com]
+  
+  #if (com.casecmp('list') == 0)
+    #List(book)
+  #elsif (com.casecmp('exit') == 0)
+    #b = 0
+  #end
+end until b == 0
+
+=begin
+while b != 0 do
+  print "Command?: "
+  com = gets.chop
+  #if com.eql?('list')
+  if (com.casecmp('list') == 0)
+    List(book)
+  #elsif com.eql?('exit')
+  elsif (com.casecmp('exit') == 0)
+    b = 0
+  end
+end
+=end
