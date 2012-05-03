c5b2cca0c01669c88ae916d08bd4c3604ffffaac
Andrew Peacock <skooljster@gmail.com>
2012-05-03 00:22:09 -0500

    Start of concentration

--- concentration/Concentration.rb
+++ concentration/Concentration.rb
@@ -1,5 +1,13 @@
+
 #MAIN
-print "What are the coordinates of the first block?: "
-fb = gets.chop
-print "What are the coordinates of the second block?: "
-sb = gets.chop
+puts "What are the coordinates of the first block?"
+print "X: "
+x1 = gets.chop
+print "Y: "
+y1 = gets.chop
+puts "What are the coordinates of the second block?"
+print "X: "
+x2 = gets.chop
+print "Y: "
+y2 = gets.chop
+Board.compare(fb, sb)
